0f3ec8ec57f4e9b8d40f32644c7649bd484900ff
skyWrightC <corey.wright@sky.uk>
2023-02-09 09:23:48 +0000

    database files added

--- src/main/java/com/sky/peopleapi/service/PersonService.java
+++ src/main/java/com/sky/peopleapi/service/PersonService.java
@@ -1,4 +1,19 @@
 package com.sky.peopleapi.service;
 
+import com.sky.peopleapi.entities.Person;
+
+import java.util.List;
+
 public interface PersonService {
+
+    Person createPerson(Person p);
+
+    Person getById(int id);
+
+    List<Person> getAll();
+
+    Person update(int id, String name, Integer age, String job);
+
+    Person remove(int id);
+
 }
---
+++ src/main/java/com/sky/peopleapi/service/PersonServiceDB.java
@@ -0,0 +1,32 @@
+package com.sky.peopleapi.service;
+
+import com.sky.peopleapi.entities.Person;
+
+import java.util.List;
+
+public class PersonServiceDB implements PersonService{
+    @Override
+    public Person createPerson(Person p) {
+        return null;
+    }
+
+    @Override
+    public Person getById(int id) {
+        return null;
+    }
+
+    @Override
+    public List<Person> getAll() {
+        return null;
+    }
+
+    @Override
+    public Person update(int id, String name, Integer age, String job) {
+        return null;
+    }
+
+    @Override
+    public Person remove(int id) {
+        return null;
+    }
+}
